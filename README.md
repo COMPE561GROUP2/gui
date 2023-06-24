44493c65e2e29b6463c3d634f71fb536e94d7ae4
CameronSettles <46801236+CameronSettles@users.noreply.github.com>
2023-06-24 15:33:28 -0700

    bug fix Changed README title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Touch Grass
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/Login.js";
 import Blog from "./pages/Blog.js";
 import Register from "./pages/Register.js";
 import Activities from "./pages/Activities.js";
-import User_page from "./pages/User_page.js";
+import UserPage from "./pages/UserPage.js";
 
 import Navbar from "./elements/Navbar.js";
 
@@ -49,7 +49,7 @@ class App extends React.Component {
               <Route exact path="blog" element={<Blog />} />
               <Route exact path="login" element={<Login />} />
               <Route exact path="register" element={<Register />} />
-              <Route exact path="user_page" element={<User_page />} />
+              <Route exact path="userpage" element={<UserPage />} />
 
               <Route path="*" element={<Navigate to="home" replace />} />
             </Routes>
