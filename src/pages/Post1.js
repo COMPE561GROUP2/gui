834106f6480a1a5e363a6e0efdc022cc7ed78657
NathanBlanchard051201 <nathan.blanchard@cox.net>
2023-07-01 16:00:30 -0700

    got rid of excess comment at the bottom of post1.js

--- src/pages/Post1.js
+++ src/pages/Post1.js
@@ -85,46 +85,4 @@ const Post1 = () => {
 );
 };
 
-/*
-export default Post1;
-
-const Post1 = () => {
-  return (
-    
-    <div>
-      <h1>Embracing Life: Maximizing the Value of Each Heartbeat</h1>
-
-      <p>
-        Introduction: <br />
-        In the grand tapestry of existence, our time on Earth is limited. Every breath we take and every beat of our heart brings us one step closer to the culmination of our journey. Understanding the finite nature of our existence compels us to embrace life fully, cherishing every moment and utilizing our precious heartbeats in the pursuit of fulfillment and purpose. This article delves into the philosophy of making the most of our limited time, focusing on the value of each heartbeat.
-      </p>
-
-      <p>
-        The Fragility of Life: <br />
-        Life, with all its beauty and wonder, is inherently delicate. The realization that our heartbeats are numbered serves as a poignant reminder of our mortality. Rather than being burdened by this truth, we can embrace it as a catalyst for action and a motivation to live purposefully. By recognizing the fragility of life, we develop a heightened appreciation for every heartbeat, propelling us towards a life of significance.
-      </p>
-
-      <p>
-        Embracing the Present Moment: <br />
-        Too often, we find ourselves preoccupied with the past or consumed by worries about the future. However, a conscious shift towards living in the present moment allows us to experience the richness and depth of existence. Each heartbeat becomes a reminder to be fully present, savoring the sights, sounds, and emotions that surround us. By immersing ourselves in the present, we unlock a gateway to joy, gratitude, and profound connections with the world around us.
-      </p>
-
-      <p>
-        Seizing Opportunities: <br />
-        Life presents us with a myriad of opportunities, both big and small, waiting to be seized. Recognizing the finite nature of our heartbeats propels us to pursue our passions, take risks, and embrace new experiences. It urges us to step outside our comfort zones, for it is in those moments of uncertainty that we often find the greatest rewards. Whether it's pursuing a dream career, embarking on an adventure, or nurturing relationships, each heartbeat urges us forward, encouraging us to make the most of the opportunities that come our way.
-      </p>
-
-      <p>
-        Living Authentically: <br />
-        In a world that often imposes expectations and societal norms, honoring our individuality and living authentically becomes a profound way to honor the finite number of heartbeats we possess. Each beat echoes the importance of embracing our true selves, unapologetically expressing our beliefs, passions, and values. By living authentically, we inspire others to do the same, creating a ripple effect that spreads positivity and encourages personal growth.
-      </p>
-
-      <p>
-        Leaving a Lasting Legacy: <br />
-        The understanding that our heartbeats are numbered provides a powerful impetus to leave a lasting impact on the world. We are not mere spectators in the grand theater of life; we are the playwrights, actors, and directors of our own narratives. Each heartbeat becomes an opportunity to contribute to something greater than ourselves, whether it's through acts of kindness, creative endeavors, or making a positive difference in the lives of others. By embracing the legacy we wish to leave behind, we infuse purpose and meaning into every beat of our hearts.
-      </p>
-    </div>
-  );
-};
-*/
 export default Post1;
