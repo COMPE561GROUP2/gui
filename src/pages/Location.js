d9b93dac3f4ecfba008ba95e43c6298003b328ff
CameronSettles <camsettles662@gmail.com>
2023-07-01 17:49:17 -0700

    fix

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -43,48 +43,111 @@ const Location = () => {
         crossOrigin="anonymous"
       />
 
-        <section>
-            <div className="container-fluid rounded pt-5">
-              <div className="card bg-dark text-white w-50 mx-auto">
-              <img src={images.surf1} className="card-img-rounded-0" alt="..." style={{height:380}}/>
-              <div className="card-img-overlay">
-                  <h1 className="card-title">{location.name}'s location</h1>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
+      <section>
+        <div className="container-fluid rounded pt-5">
+          <div className="card bg-dark text-white w-50 mx-auto">
+            <img
+              src={images.surf1}
+              className="card-img-rounded-0"
+              alt="..."
+              style={{ height: 380 }}
+            />
+            <div className="card-img-overlay">
+              <h1 className="card-title">{location.name}'s location</h1>
+              
+            </div>
 
-              <div className="card rounded-0">
+            <div className="card rounded-0">
               <div className="card-header w-100 mx-auto">
                 <ul className="nav nav-tabs card-header-tabs">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="true" data-bs-toggle="tab" href="#Description">Description</a>
+                    <a
+                      className="nav-link active"
+                      aria-current="true"
+                      data-bs-toggle="tab"
+                      href="#Description"
+                    >
+                      Description
+                    </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" data-bs-toggle="tab" href="#Waypoint">Waypoints</a>
+                    <a
+                      className="nav-link"
+                      data-bs-toggle="tab"
+                      href="#Waypoint"
+                    >
+                      Waypoints
+                    </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" data-bs-toggle="tab" href="#Conditions">Conditions</a>
+                    <a
+                      className="nav-link"
+                      data-bs-toggle="tab"
+                      href="#Conditions"
+                    >
+                      Conditions
+                    </a>
                   </li>
                 </ul>
-                </div>
-                <form className="card-body tab-content" style={{height:400}}>
+              </div>
+              <form className="card-body tab-content" style={{ height: 400 }}>
                 <div className="tab-pane active" id="Description">
-                    <p className="card-text">Brief summary here about the location</p>
+                  <p className="card-text">
+                    Nestled along the picturesque coastline of San Diego,
+                    California, La Jolla Beach is a coastal gem that epitomizes
+                    Southern California's charm. With its pristine sandy shores,
+                    crystal-clear turquoise waters, and rugged cliffs, La Jolla
+                    Beach captivates visitors with its natural beauty. This
+                    idyllic stretch of coastline offers a range of activities
+                    for beachgoers, from sunbathing and picnicking to swimming
+                    and surfing in the Pacific Ocean's refreshing waves.
+                  </p>
                 </div>
                 <div className="tab-pane" id="Waypoint">
-                    <p className=" card-text">Brief summary here about the location's waypoint or map informations</p>
+                  <p className=" card-text">
+                    To get from San Diego State University (SDSU) to La Jolla
+                    Beach, you can follow these directions: Head west on
+                    Montezuma Rd toward College Ave. Continue on Montezuma Rd as
+                    it turns into Campanile Dr. Take a slight right onto College
+                    Ave. Merge onto I-8 West via the ramp on the left. Continue
+                    on I-8 West for approximately 5 miles. Take the exit for I-5
+                    North toward Los Angeles. Merge onto I-5 North and continue
+                    for about 10 miles. Take exit 28B for La Jolla Village Dr.
+                    Keep left at the fork and follow signs for La Jolla Village
+                    Dr West. Merge onto La Jolla Village Dr. Continue on La
+                    Jolla Village Dr for approximately 2 miles. Turn right onto
+                    Torrey Pines Rd. Follow Torrey Pines Rd for about 2.5 miles
+                    until you reach La Jolla Shores Dr. Turn left onto La Jolla
+                    Shores Dr. Continue on La Jolla Shores Dr for approximately
+                    0.7 miles, and you will reach La Jolla Beach. These
+                    directions should take you from SDSU to La Jolla Beach, but
+                    keep in mind that traffic conditions and road closures can
+                    affect the journey, so it's always a good idea to check for
+                    updates before you start your trip.
+                  </p>
                 </div>
                 <div className="tab-pane" id="Conditions">
-                    <p className=" card-text">Brief summary here about the location's weather</p>
+                  <p className=" card-text">
+                    Today in La Jolla Beach, the weather is partly cloudy with a
+                    gentle breeze blowing in from the west. The temperature is a
+                    comfortable 70 degrees Fahrenheit (21 degrees Celsius). As
+                    you walk along the sandy shore, you'll feel the warmth of
+                    the sun on your skin, with occasional breaks of shade as
+                    fluffy white clouds pass by. The sea is calm, inviting you
+                    for a refreshing swim or a leisurely walk along the water's
+                    edge. It's a great day to soak up the coastal ambiance and
+                    enjoy the beauty of La Jolla Beach.
+                  </p>
                 </div>
-            </form>
-                {/* <form className="card-body tab-content" style={{height:400}}>
+              </form>
+              {/* <form className="card-body tab-content" style={{height:400}}>
                   <h5 className="card-title">Brief summary here about the location</h5>
                   <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 </form> */}
-              </div>
-              </div>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
